adb8a235c60d77e25f377e71f55067947ce57094
galijander <galicstrahinja@gmail.com>
2024-11-10 21:45:00 +0100

    fix/ improved security meassures

--- src/main/java/com/onlybuns/onlybuns/Model/User.java
+++ src/main/java/com/onlybuns/onlybuns/Model/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,6 +31,11 @@ public class User {
     
     private String password;
     private String name;
+    
+    @Pattern(
+        regexp = "^[A-Za-z\\s]+,[A-Za-z\\s]+,[A-Za-z\\s]+,\\d+$", 
+        message = "Address format should be 'country,city,street,number'"
+    )
     private String address;
     private boolean isActive;
     
